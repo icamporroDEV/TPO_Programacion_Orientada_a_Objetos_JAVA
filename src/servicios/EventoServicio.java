9cabcb9d250e1f967bf88823f64d02a0695ae494
Octavio C <octavioc.cuenta@gmail.com>
2024-11-12 09:06:38 -0300

    Mejora_Modificacion_Evento
    Agregamos un swtch en la modificación de eventos para darle al usuario mas flexibilidad para elejir. No hace falta modificar todo ahora

--- src/servicios/EventoServicio.java
+++ src/servicios/EventoServicio.java
@@ -35,13 +35,29 @@ public class EventoServicio {
         Evento evento = buscarEventoPorNombre(nombre);
         if (evento != null) {
             System.out.println("Evento encontrado: " + evento.getNombre());
+            System.out.println("===== Elija Modificación =====");
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Ingrese la nueva fecha del evento: ");
-            evento.setFecha(scanner.nextLine());
-            System.out.print("Ingrese la nueva ubicación del evento: ");
-            evento.setUbicacion(scanner.nextLine());
-            System.out.print("Ingrese la nueva descripción del evento: ");
-            evento.setDescripcion(scanner.nextLine());
+            System.out.println("1. Modificar Fecha");
+            System.out.println("2. Modificar Ubicación");
+            System.out.println("3. Modificar Descripción");
+            System.out.print("Seleccione una opción: ");
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion){
+                case 1:
+                    System.out.print("Ingrese la nueva fecha del evento: ");
+                    evento.setFecha(scanner.nextLine());
+                    break;
+                case 2:
+                    System.out.print("Ingrese la nueva ubicación del evento: ");
+                    evento.setUbicacion(scanner.nextLine());
+                    break;
+                case 3:
+                    System.out.print("Ingrese la nueva descripción del evento: ");
+                    evento.setDescripcion(scanner.nextLine());
+                    break;
+            }
             System.out.println("Evento modificado exitosamente.");
         } else {
             System.out.println("Evento '" + nombre + "' no encontrado.");
